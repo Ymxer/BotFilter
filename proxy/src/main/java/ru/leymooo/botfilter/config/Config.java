9466da41dd562bc7df496d7503de6dfaa7f2a3e3
Loyisa <loyisa893@gmail.com>
2022-12-18 21:18:10 +0800

    Add untranslated strings

--- proxy/src/main/java/ru/leymooo/botfilter/config/Config.java
+++ proxy/src/main/java/ru/leymooo/botfilter/config/Config.java
@@ -93,7 +93,7 @@ public class Config
             }
         } catch ( IOException ex )
         {
-            BungeeCord.getInstance().getLogger().log( Level.WARNING, "[BotFilter] Не могу загрузить конфиг ", ex );
+            BungeeCord.getInstance().getLogger().log( Level.WARNING, "[BotFilter] Can't load the config ", ex );
             return false;
         }
         set( yml, "" );
